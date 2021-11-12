3340b5a18b35646dde1aeb6486f8f835b268c099
anafink <anastassia.fink@stud.uni-heidelberg.de>
2021-11-12 13:17:55 +0100

    Day 3

--- notebooks/exercises/day 3/closest_citys.py
+++ notebooks/exercises/day 3/closest_citys.py
@@ -5,9 +5,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-with open(path, mode='r') as cities:
-    reader = csv.reader(cities)
-    citydict = {rows[0]:rows[2] for rows in reader}
 
+def create_data_stucture(map_data_file):
+    cities_dic = pd.read_csv(map_data_file)
+    return cities_dic
 
-print(citydict)
+def find_closest_cities(point, data_structure):
+    distances = []
+    for lat, lng in zip(data_structure['lat'].tolist(), data_structure['lng'].tolist()) :
+        distances.append(math.dist(point,(lat,lng)))
+    data_structure["Distance"] = distances
+    data_structure = data_structure.sort_values(by = "Distance")
+    closest_cities = data_structure.head(10)
+    return closest_cities
+
+hd = (49.4, 8.67) #Heidelberg
+data_structure = create_data_stucture(path)
+print(find_closest_cities(hd, data_structure))
