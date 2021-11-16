d33c08f224d21126688d4c703d71e2badc2f037e
anafink <anastassia.fink@stud.uni-heidelberg.de>
2021-11-16 11:35:27 +0100

    day 5 update

--- notebooks/exercises/tests/test_proetin_class.py
+++ notebooks/exercises/tests/test_proetin_class.py
@@ -4,4 +4,6 @@ from pathlib import Path
 import protein_class
 
 def test_protein_class():
-    ex_protein = ("Some Protein", 1234, [A,C,D,E,F,G,H,I,K,L,M,N,P,Q,R,S,T,V,W,Y])
+    ex_protein = protein("Some Protein", 1234, ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"])
+    ex_plot = ex_protein.plot
+    assert ex_plot == open("notebooks/exercises/day 5/example_protein.png")
