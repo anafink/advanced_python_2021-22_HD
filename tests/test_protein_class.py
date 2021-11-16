8788db5d43cb3ac5565b4f646054e777c5864adb
anafink <anastassia.fink@stud.uni-heidelberg.de>
2021-11-16 13:11:03 +0100

    day 5 update 1.3

--- tests/test_protein_class.py
+++ tests/test_protein_class.py
@@ -7,7 +7,12 @@ sys.path.append(
 
 from protein_class import protein
 
-def test_protein_class():
+def test_protein_plot():
     ex_protein = protein("Some Protein", 1234, ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"])
     ex_plot = ex_protein.plot
     assert ex_plot == open("../notebooks/exercises/day 5/example_protein.png")
+
+def test_protein_pos():
+    ex_protein = protein("Some Protein", 1234, ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"])
+    ex_pos = ex_protein.get_aa_pos
+    assert ex_pos == list(range(1,len(["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"])+1))
