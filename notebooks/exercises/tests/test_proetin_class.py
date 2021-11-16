3e5a478b719eeb9e9eb45f2c0ee5731c50ae3af2
anafink <anastassia.fink@stud.uni-heidelberg.de>
2021-11-16 11:38:52 +0100

    day 5 updatte 1.1

--- notebooks/exercises/tests/test_proetin_class.py
+++ notebooks/exercises/tests/test_proetin_class.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-import protein_class
+from protein_class import protein
 
 def test_protein_class():
     ex_protein = protein("Some Protein", 1234, ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"])
