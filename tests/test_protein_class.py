3b647e2c7e6963ee4fcaad64c2b967f1b2d99f93
anafink <anastassia.fink@stud.uni-heidelberg.de>
2021-12-29 16:35:08 +0100

    update

--- tests/test_protein_class.py
+++ tests/test_protein_class.py
@@ -8,7 +8,7 @@ sys.path.append(
 from protein_class.protein_class import Protein
 
 
-def test_protein_pos():
+
+def test_protein_sequence():
     ex_protein = Protein("Some Protein", "1234", ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"])
-    ex_pos = ex_protein.get_aa_pos
-    assert ex_pos == ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
+    assert ex_protein.sequence == ["A","C","D","E","F","G","H","I","K","L","M","N","P","Q","R","S","T","V","W","Y"]
